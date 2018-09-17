47d3a6ed80b9e6793fa931503b4e6440aedb25e4
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-18 00:07:34 +0300

    pinguins

--- pinguins/pinguins.py
+++ pinguins/pinguins.py
@@ -5,14 +5,14 @@ def main():
     n = int(input())
     s1 = '   _~_    '
     s2 = '  (o o)   '
-    s3 = ' /  V  \  '
-    s4 = '/(  _  )\ '
+    s3 = ' /  V  \\  '
+    s4 = '/(  _  )\\ '
     s5 = '  ^^ ^^   '
-    print(s1*n)
-    print(s2*n)
-    print(s3*n)
-    print(s4*n)
-    print(s5*n)
+    print(s1 * n)
+    print(s2 * n)
+    print(s3 * n)
+    print(s4 * n)
+    print(s5 * n)
 
 
 if __name__ == '__main__':
