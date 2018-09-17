aca05106bac5ade2f650a91d3b0f58fa46238ba0
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-18 01:17:46 +0300

    fizz_buzz

--- fizz_buzz/fizz_buzz.py
+++ fizz_buzz/fizz_buzz.py
@@ -13,11 +13,11 @@ def get_fizz_buzz(n):
             "Fizz Buzz", else - value.
     """
     myList = list(range(1, n + 1))
-    for i in range(3, n, 3):
+    for i in range(3, n + 1, 3):
         myList[i - 1] = 'Fizz'
-    for i in range(5, n, 5):
+    for i in range(5, n + 1, 5):
         myList[i - 1] = 'Buzz'
-    for i in range(15, n, 15):
+    for i in range(15, n + 1, 15):
         myList[i - 1] = 'Fizz Buzz'
     return myList
 
