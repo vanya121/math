d0e1e93f314e59cc83479bedb08d415c4ca51388
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-18 01:12:05 +0300

    fizz_buzz

---
+++ fizz_buzz/fizz_buzz.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from typing import List, TypeVar
+
+FizzBuzzType = TypeVar('FizzBuzzType', int, str)
+
+
+def get_fizz_buzz(n):
+    """
+    :param n: size of sequence
+    :return: list of values. If value divided by 3 - "Fizz",
+    if value divided by 5 - "Buzz", if value divided by 15 -
+            "Fizz Buzz", else - value.
+    """
+    myList = list(range(1, n + 1))
+    for i in range(3, n, 3):
+        myList[i - 1] = 'Fizz'
+    for i in range(5, n, 5):
+        myList[i - 1] = 'Buzz'
+    for i in range(15, n, 15):
+        myList[i - 1] = 'Fizz Buzz'
+    return myList
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(get_fizz_buzz(n))
