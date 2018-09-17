375720d5b05c84b74849022b8aa1d82be60c1f1d
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-17 20:24:53 +0300

    hello_world

---
+++ hello_world/hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+def get_hello_world():
+    """
+    :return: hello world string
+    """
+    return "Hello world!"
+
+
+if __name__ == "__main__":
+    print(get_hello_world())
