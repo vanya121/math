cacbf37e2bc45e1ba5187a78d6a3b2bf20310c93
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-17 21:12:54 +0300

    pinguins

---
+++ pinguins/pinguins.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+def main():
+    n = int(input())
+    s1 = '   _~_    '
+    s2 = '  (o o)   '
+    s3 = ' /  V  \  '
+    s4 = '/(  _  )\ '
+    s5 = '  ^^ ^^   '
+    print(s1*n)
+    print(s2*n)
+    print(s3*n)
+    print(s4*n)
+    print(s5*n)
+
+
+if __name__ == '__main__':
+    main()
