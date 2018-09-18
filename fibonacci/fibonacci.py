5f6e50cd25d334dafc39fb7147953910cfc32f58
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-18 03:34:21 +0300

    fibonacci

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 
-def get_fibonacci_value(first, second, position_value):
+def get_fibonacci_value(position_value, first, second):
     """
     :param position:  Position in fibonacci sequence
     :param first_value: First value of fibonacci sequence
     :param second_value: Second value of fibonacci sequence
     :return: value of fibonacci sequence on given position
     """
-    if position_value == 1:
+    if position_value == 0:
         return first
-    if position_value == 2:
+    if position_value == 1:
         return second
-    cnt = 3
+    cnt = 2
     value = first + second
     while cnt < position_value:
         first = second
@@ -23,5 +23,5 @@ def get_fibonacci_value(first, second, position_value):
 
 
 if __name__ == "__main__":
-    first, second, position_value = int(input()), int(input()), int(input())
-    print(get_fibonacci_value(first, second, position_value))
+    position_value, first, second = int(input()), int(input()), int(input())
+    print(get_fibonacci_value(position_value, first, second))
