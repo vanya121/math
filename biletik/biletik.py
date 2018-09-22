293b376fd32711251c0d1b3963d76765c2eced94
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-23 02:07:16 +0300

    biletik

--- biletik/biletik.py
+++ biletik/biletik.py
@@ -15,14 +15,29 @@ def is_happy_ticket(current_ticket):
 
 
 def get_nearest_happy_ticket(current_ticket):
+    firt_zero = 0
+    if current_ticket[0] == '0':
+        current_ticket = int(current_ticket)
+        current_ticket = current_ticket + 100001
+        firt_zero = 1
     ticket = int(current_ticket)
     cnt = 0
     while True:
         if is_happy_ticket(ticket + cnt) is True:
-            happy_ticket = str(ticket + cnt)
+            if firt_zero == 1:
+                happy_ticket = str(ticket + cnt - 100001)
+                while len(happy_ticket) < 6:
+                    happy_ticket = '0' + happy_ticket
+            else:
+                happy_ticket = str(ticket + cnt)
             return happy_ticket
         if is_happy_ticket(ticket - cnt) is True:
-            happy_ticket = str(ticket - cnt)
+            if firt_zero == 1:
+                happy_ticket = str(ticket - cnt - 100001)
+                while len(happy_ticket) < 6:
+                    happy_ticket = '0' + happy_ticket
+            else:
+                happy_ticket = str(ticket - cnt)
             return happy_ticket
         cnt += 1
 
