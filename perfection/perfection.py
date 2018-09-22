79266ed8f171e8547c7f035533768e5fde9a5750
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-23 00:15:22 +0300

    perfection

--- perfection/perfection.py
+++ perfection/perfection.py
@@ -5,13 +5,18 @@ import math
 def is_perfect(num):
     p = 1
     if num % 2 == 1:
-        return "False"
+        return False
     while (2 ** (p - 1)) * (2 ** p - 1) < num:
         p += 1
     if (2 ** (p - 1)) * (2 ** p - 1) == num:
         b = int(math.sqrt(2 ** p - 1))
         for i in range(2, b):
             if (2 ** p - 1) % i == 0:
-                return "False"
-        return "True"
-    return "False"
+                return False
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(is_perfect(n))
--- perfection/test_public.py
+++ perfection/test_public.py
@@ -3,19 +3,3 @@ from perfection import is_perfect
 
 def test_example1():
     assert is_perfect(6)
-
-
-def test_example2():
-    assert is_perfect(496)
-
-
-def test_example3():
-    assert is_perfect(1)
-
-
-def test_example4():
-    assert is_perfect(28)
-
-
-def test_example5():
-    assert is_perfect(2)
