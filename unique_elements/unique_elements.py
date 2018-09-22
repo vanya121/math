995458c3a284a57f5b23c1624ce9efaff53fb9d6
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-22 21:06:18 +0300

    unique_elements

--- unique_elements/unique_elements.py
+++ unique_elements/unique_elements.py
@@ -2,9 +2,11 @@
 
 
 def uniq(elements):
+    ans = list()
     while len(elements) > 0:
         element = elements[0]
-        if elements.count(element) > 1:
-            for i in range(elements.count(element)):
-                elements.remove(element)
-    return elements
+        if elements.count(element) == 1:
+            ans.append(element)
+        for i in range(elements.count(element)):
+            elements.remove(element)
+    return ans
