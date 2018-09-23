93a25971893dcc4736a4417b2e22e15b4ca46639
Krutov Ivan <iwankrutov@yandex.ru>
2018-09-23 03:08:41 +0300

    quick_sort

--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -7,12 +7,13 @@ def quick_sort(data):
     right = list()
     if len(data) <= 1:
         return data
+    reference_element = data[len(data)//2]
     for i in data:
-        if i < data[0]:
+        if i < reference_element:
             left.append(i)
-        if i == data[0]:
+        if i == reference_element:
             stand.append(i)
-        if i > data[0]:
+        if i > reference_element:
             right.append(i)
     return quick_sort(left) + stand + quick_sort(right)
 
