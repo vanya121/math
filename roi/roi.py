736da30027fae98dfa7fa6c2cb657d4d30654580
Krutov Ivan <iwankrutov@yandex.ru>
2018-10-09 14:11:33 +0300

    roi

--- roi/roi.py
+++ roi/roi.py
@@ -8,10 +8,8 @@ def compute_roi(
     answer = 0
     sum_inv = 0
     for key in investments:
-        coi = coins[(coins['symbol'] == key) &
-        ((coins['date'] == start_date) | (coins['date'] == end_date))]
+        coi = coins[(coins['symbol'] == key) & ((coins['date'] == start_date) | (coins['date'] == end_date))]
         seq = coi['price']
-        answer = answer + (investments[key] *
-        (seq[end_date] - seq[start_date]) / seq[start_date])
+        answer = answer + (investments[key] * (seq[end_date] - seq[start_date]) / seq[start_date])
         sum_inv = sum_inv + investments[key]
     return answer / sum_inv
