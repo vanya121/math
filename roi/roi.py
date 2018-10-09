197b9110a8e3251cca876a1d94ed386e2d828b05
Krutov Ivan <iwankrutov@yandex.ru>
2018-10-09 14:08:30 +0300

    roi

---
+++ roi/roi.py
@@ -0,0 +1,17 @@
+#!usr/bin/python
+import pandas as pd
+
+
+def compute_roi(
+    coins: pd.DataFrame, investments: dict, start_date: str, end_date: str
+):
+    answer = 0
+    sum_inv = 0
+    for key in investments:
+        coi = coins[(coins['symbol'] == key) &
+        ((coins['date'] == start_date) | (coins['date'] == end_date))]
+        seq = coi['price']
+        answer = answer + (investments[key] *
+        (seq[end_date] - seq[start_date]) / seq[start_date])
+        sum_inv = sum_inv + investments[key]
+    return answer / sum_inv
