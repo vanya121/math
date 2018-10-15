5c8c6f9195232f82992cd6f8e166152248e6b5dc
Krutov Ivan <iwankrutov@yandex.ru>
2018-10-16 02:11:42 +0300

    vm

--- vm/cases.py
+++ vm/cases.py
@@ -11,35 +11,35 @@ TEST_CASES = [
 print(17)
 """),
     Case(
-        name="bbb",
+        name="1",
         text_code=r"""
 a = 1
 b = 2
 print(a + b)
 """),
     Case(
-        name="bbb2",
+        name="2",
         text_code=r"""
 a = 1
 b = 2
 print(a * b)
 """),
     Case(
-        name="bbb3",
+        name="3",
         text_code=r"""
 a = 2
 b = 3
 print(a ** b)
 """),
     Case(
-        name="QWERR",
+        name="4",
         text_code=r"""
 a = 2
 print(a ** 2)
 """),
 
     Case(
-        name="const2",
+        name="5",
         text_code=r"""
 print(2 ** 3)
 """),
@@ -48,6 +48,13 @@ print(2 ** 3)
         text_code=r"""
 d = {1: 2, 3: 4}
 print(d[1])
+"""),
+    Case(
+        name="test102",
+        text_code=r"""
+d = 9
+d *= 3 
+print(d)
 """),
     Case(
         name="test61",
@@ -67,5 +74,24 @@ try:
     print("Shouldn't be here...")
 except IndexError:
     print("caught it!")
+"""),
+    Case(
+        name="QWERR2",
+        text_code=r"""
+print(3 > 2)
+"""),
+    Case(
+        name="12",
+        text_code=r"""
+a = 1
+a = a + 1
+print(a)
+"""),
+    Case(
+        name="13",
+        text_code=r"""
+a = 1
+a += 1
+print(a)
 """)
 ]
--- vm/vm.py
+++ vm/vm.py
@@ -26,73 +26,98 @@ class VirtualMachine:
                     self.dic[item.argval] = item.argval
                 self.stack.append(item.argval)
 
-            if item.opname == 'BINARY_ADD':
+            if item.opname == 'BINARY_ADD' or item.opname == 'INPLACE_ADD':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS + TOS1)
 
-            if item.opname == 'BINARY_FLOOR_DIVIDE':
+            if item.opname == 'BINARY_FLOOR_DIVIDE' or item.opname == 'INPLACE_ADD':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 // TOS)
 
-            if item.opname == 'BINARY_LSHIFT':
+            if item.opname == 'BINARY_LSHIFT' or item.opname == 'INPLACE_ADD':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 << TOS)
 
-            if item.opname == 'BINARY_AND':
+            if item.opname == 'BINARY_AND' or item.opname == 'INPLACE_AND':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 & TOS)
 
-            if item.opname == 'BINARY_MODULO':
+            if item.opname == 'BINARY_MODULO' or item.opname == 'INPLACE_MODULO':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 % TOS)
 
-            if item.opname == 'BINARY_MULTIPLY':
+            if item.opname == 'BINARY_MULTIPLY' or item.opname == 'INPLACE_MULTIPLY':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 * TOS)
 
-            if item.opname == 'BINARY_OR':
+            if item.opname == 'BINARY_OR' or item.opname == 'INPLACE_OR':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 | TOS)
 
-            if item.opname == 'BINARY_POWER':
+            if item.opname == 'BINARY_POWER' or item.opname == 'INPLACE_POWER':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 ** TOS)
 
-            if item.opname == 'BINARY_RSHIFT':
+            if item.opname == 'BINARY_RSHIFT' or item.opname == 'INPLACE_RSHIFT':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 >> TOS)
 
-            if item.opname == 'BINARY_RSHIFT':
+            if item.opname == 'BINARY_MATRIX_MULTIPLY' or item.opname == 'INPLACE_MATRIX_MULTIPLY':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
-                self.stack.append(TOS1 >> TOS)
+                self.stack.append(TOS1 @ TOS)
 
-            if item.opname == 'BINARY_SUBTRACT':
+            if item.opname == 'BINARY_SUBTRACT' or item.opname == 'INPLACE_SUBTRACT':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 - TOS)
 
-            if item.opname == 'BINARY_TRUE_DIVIDE':
+            if item.opname == 'BINARY_TRUE_DIVIDE' or item.opname == 'INPLACE_TRUE_DIVIDE':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 / TOS)
 
-            if item.opname == 'BINARY_XOR':
+            if item.opname == 'BINARY_XOR' or item.opname == 'INPLACE_XOR':
                 TOS = self.dic[self.stack.pop()]
                 TOS1 = self.dic[self.stack.pop()]
                 self.stack.append(TOS1 ^ TOS)
 
+            if item.opname == 'BINARY_SUBSCR':
+                TOS = self.dic[self.stack.pop()]
+                TOS1 = self.dic[self.stack.pop()]
+                self.stack.append(TOS1[TOS])
+
+            if item.opname == 'COMPARE_OP':
+                a = self.stack.pop()
+                b = self.stack.pop()
+                if item.argval == '>':
+                    self.stack.append(a < b)
+                if item.argval == '<':
+                    self.stack.append(a > b)
+                if item.argval == '==':
+                    self.stack.append(a == b)
+                if item.argval == '!=':
+                    self.stack.append(a != b)
+                if item.argval == '<=':
+                    self.stack.append(a >= b)
+                if item.argval == '>=':
+                    self.stack.append(a <= b)
+
             if item.opname == 'CALL_FUNCTION':
-                arg = self.stack.pop()
+                z = self.stack.pop()
+                if z in self.dic:
+                    arg = self.dic[z]
+                else:
+                    arg = z
                 func = self.stack.pop()
                 f = builtins.__dict__[func](arg)
                 self.stack.append(f)
